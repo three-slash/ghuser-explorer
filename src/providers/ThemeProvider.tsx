e5df6e106b169dde11f777cead080b3a2bd96c80
Ribhararnus Pracutian <oknoorap@gmail.com>
2025-06-30 21:59:56 +0700

    refactor: move ThemeProvider implementation from components to providers directory

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,1 +1,23 @@
-export { ThemeProvider } from '../components/ThemeProvider';
+import { useThemeStore } from '@/store/theme';
+import React, { useEffect } from 'react';
+
+export interface ThemeProviderProps {
+	children: React.ReactNode;
+}
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+	const theme = useThemeStore((state) => state.theme);
+
+	useEffect(() => {
+		const root = window.document.documentElement;
+		root.classList.remove('light', 'dark');
+		if (theme === 'system') {
+			const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+			root.classList.add(systemTheme);
+		} else {
+			root.classList.add(theme);
+		}
+	}, [theme]);
+
+	return <>{children}</>;
+};
